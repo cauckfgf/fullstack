bcb58163283813a975a757c53b5360c1ea4d9d9b
fgf <fgf@bimsheng.com>
2019-08-05 13:40:52 +0800

    yq

--- docker-com/yuanqu/static/js/routes/system.js
+++ docker-com/yuanqu/static/js/routes/system.js
@@ -606,7 +606,7 @@ const system = {
                 for(var i in res.data){
                       var d = res.data[i]
                       coords = d.path_list
-                      var zlevel = 1
+                      var zlevel = 3
                       if(!d.show_direction){
                         zlevel=2
                       }
