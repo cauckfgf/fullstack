431d5423756765d8f6c5a054b30d5be4bda3dd4d
fgf <fgf@bimsheng.com>
2019-09-17 19:03:58 +0800

    bj

---
+++ docker/binjiang/cron1.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+# 1小时填充一次SensorDataTime
+import os
+import sys
+import django
+import json
+import httplib
+import datetime
+import traceback
+from dateutil.relativedelta import relativedelta
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "binjiang.settings")
+django.setup()
+
+from Device.models  import *
+def sensordatatimetask():
+    now = datetime.datetime.now()
+    # 前一小时
+    t = now + relativedelta(hours=-1)
+
+    querysetlist = []
+    sensordata = SensorData.objects.filter(stime__gt=t).values('sensor','stime').annotate(Max('data'))
+    middle = {}
+    for each in sensordata:
+        if middle.get(each['sensor'], None):
+            middle[each['sensor']] = (each['data__max'], each['stime'])
+
+    for key,value in middle.items():
+        t = SensorDataTime()
+        t.sensor = Sensor.objects.get(id=key)
+        t.data = value[0]
+        t.stime = value[1]
+        querysetlist.append(t)
+    SensorDataTime.objects.bulk_create(querysetlist)
