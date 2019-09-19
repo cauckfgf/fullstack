593bb3b7b4dca2ff30f7eb2c59618d424374774e
fgf <fgf@bimsheng.com>
2019-09-19 16:14:26 +0800

    bj

--- docker/binjiang/getlast.py
+++ docker/binjiang/getlast.py
@@ -105,7 +105,7 @@ def on_message(client, userdata, msg):
                     cdata.voltage_C = value
                 elif name=='activeElectricalEnergy':
                     cdata.quantity = value
-            if cdata.quantity:
+            if cdata.quantity is not None:
                 cdata.save()
             circuit.lasthour()
                     
