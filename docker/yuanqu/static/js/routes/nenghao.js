0b3e82e76fba6a2ed62facd30bfeb60501bcd221
fgf <fgf@bimsheng.com>
2019-10-10 17:03:30 +0800

    yq

--- docker/yuanqu/static/js/routes/nenghao.js
+++ docker/yuanqu/static/js/routes/nenghao.js
@@ -17,7 +17,7 @@ const nenghao = {
                         <Icon type="ios-outlet"></Icon>
                         {{ item.name }}
                     </p>
-                    <i-switch slot="extra" v-model="item.switch" @on-change="change(item,$status)" >
+                    <i-switch slot="extra" v-model="item.switch" @on-change="change(...arguments,item)" >
                         <span slot="open">开启</span>
                         <span slot="close">关闭</span>
                     </i-switch>
@@ -61,7 +61,7 @@ const nenghao = {
                 window.setTimeout(this.init,60000)
             })
         },
-        change(item,status){
+        change(status,item){
             if(status){
                 ajax.get(`/device/rest/device/${item.id}/switch_on/`).then(res => {
 
