3e2966f273d99d6a9daaa068b66266e462f067a1
fgf <fgf@bimsheng.com>
2019-10-11 11:00:34 +0800

    yq

--- docker/yuanqu/yuanqu/views.py
+++ docker/yuanqu/yuanqu/views.py
@@ -102,9 +102,9 @@ def wx(request):
                     # wx_wechat.subscribe(getWxUserInfo(msg.source))
                     articles = [
                         {
-                            'title': 'test',
-                            'description': 'test',
-                            'image': '',
+                            'title': 'wifi插座管理',
+                            'description': 'wifi插座管理',
+                            'image': 'https://mmbiz.qpic.cn/mmbiz_png/uvEWHtO4sbiaCUyibY9UyHqD1y5Fe5FEZavZ701bKTRRjLxDUXnNYeZT9Yf6DdwCbic70QWerRdfFKLWaR3QWiahjQ/0?wx_fmt=png',
                             'url': 'http://energy.shuhuhu.com/index/#/nenghao'
                         },
                         # add more ...
