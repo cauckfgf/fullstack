3af6e4e76c20c7f89c0eec0e7c88434ff03aa6a8
fgf <fgf@bimsheng.com>
2019-10-21 16:06:28 +0800

    yq

--- docker/yuanqu/static/js/index.js
+++ docker/yuanqu/static/js/index.js
@@ -464,7 +464,7 @@ function initVue() {
                     }
                 },
                 option2 : {
-                    color:['rgb(5, 39, 175,0.5)','#f5b03188','#fad79788','#59ccf788','#c3b4df88'],
+                    color:['#eb2100', '#eb3600', '#d0570e', '#d0a00e', '#34da62', '#00e9db', '#00c0e9', '#0096f3', '#33CCFF', '#33FFCC'],
                     title: {
                         text: '工单数量',
                         // subtext: 'Source: https://worldcoffeeresearch.org/work/sensory-lexicon/',
@@ -495,51 +495,119 @@ function initVue() {
                         bottom: '3%',
                         containLabel: true
                     },
-                    xAxis:  {
-                        type: 'value'
-                    },
-                    yAxis: {
-                        type: 'category',
-                        data: ['北京','太原','苏州','东莞','成都']
-                    },
-                    series: [
-                        {
-                            name: '空调',
+                     xAxis: [{
+                        show: false,
+                    }],
+                    yAxis: [{
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        offset: '27',
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '16',
+                            }
+                        },
+                        data: ['北京', '苏州', '太原', '成都', '东莞']
+                    }, {
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '16',
+                            }
+                        },
+                        data: ['5', '4', '3', '2', '1']
+                    }, {
+                        name: '工单数量',
+                        nameGap: '50',
+                        nameTextStyle: {
+                            color: '#ffffff',
+                            fontSize: '16',
+                        },
+                        axisLine: {
+                            lineStyle: {
+                                color: 'rgba(0,0,0,0)'
+                            }
+                        },
+                        data: [],
+                    }],
+                    series: [{
+                            name: '条',
                             type: 'bar',
-                            stack: '总量',
+                            yAxisIndex: 0,
+                            data: [44, 50, 52, 60, 72],
                             label: {
                                 normal: {
                                     show: true,
-                                    position: 'insideRight'
+                                    position: 'right',
+                                    textStyle: {
+                                        color: '#ffffff',
+                                        fontSize: '16',
+                                    }
                                 }
                             },
-                            data: [320, 302, 301, 334, 390]
+                            barWidth: 12,
+                            itemStyle: {
+                                normal: {
+                                    color: function(params) {
+                                        var num = myColor.length;
+                                        return myColor[params.dataIndex % num]
+                                    },
+                                }
+                            },
+                            z: 2
+                        }, {
+                            name: '白框',
+                            type: 'bar',
+                            yAxisIndex: 1,
+                            barGap: '-100%',
+                            data: [ 99.5, 99.5, 99.5, 99.5, 99.5],
+                            barWidth: 20,
+                            itemStyle: {
+                                normal: {
+                                    color: '#0e2147',
+                                    barBorderRadius: 5,
+                                }
+                            },
+                            z: 1
+                        }, {
+                            name: '外框',
+                            type: 'bar',
+                            yAxisIndex: 2,
+                            barGap: '-100%',
+                            data: [ 100, 100, 100, 100, 100],
+                            barWidth: 24,
+                            itemStyle: {
+                                normal: {
+                                    color: function(params) {
+                                        var num = myColor.length;
+                                        return myColor[params.dataIndex % num]
+                                    },
+                                    barBorderRadius: 5,
+                                }
+                            },
+                            z: 0
                         },
                         {
-                            name: '空压机',
-                            type: 'bar',
-                            stack: '总量',
-                            label: {
+                            name: '外圆',
+                            type: 'scatter',
+                            hoverAnimation: false,
+                            data: [ 0, 0, 0, 0, 0],
+                            yAxisIndex: 2,
+                            symbolSize: 35,
+                            itemStyle: {
                                 normal: {
-                                    show: true,
-                                    position: 'insideRight'
+                                    color: function(params) {
+                                        var num = myColor.length;
+                                        return myColor[params.dataIndex % num]
+                                    },
+                                    opacity: 1,
                                 }
                             },
-                            data: [120, 132, 101, 134, 90]
-                        },
-                        {
-                            name: '污水',
-                            type: 'bar',
-                            stack: '总量',
-                            label: {
-                                normal: {
-                                    show: true,
-                                    position: 'insideRight'
-                                }
-                            },
-                            data: [220, 182, 191, 234, 290]
-                        },
-                       
+                            z: 2
+                        }
                     ]
                 },
                 option3 : {
