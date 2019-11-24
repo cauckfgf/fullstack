26d6eef477a03fe31db3e4f948a434294b50658d
fgf <fgf@bimsheng.com>
2019-11-24 19:42:39 +0800

    yq

---
+++ docker/yuanqu/static/keji/precache-manifest.5e6a9b3be7ed2c9a395276d3cc07cc11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d039594d491d1060e829fe6fe6a6412a",
+    "url": "/static/keji/index.html"
+  },
+  {
+    "revision": "9b0bd1a39a43cf968d5e",
+    "url": "/static/keji/static/css/2.33b2dd73.chunk.css"
+  },
+  {
+    "revision": "79b47e2436f9cb1b0bdc",
+    "url": "/static/keji/static/css/main.7efa3b5e.chunk.css"
+  },
+  {
+    "revision": "9b0bd1a39a43cf968d5e",
+    "url": "/static/keji/static/js/2.7ceb2085.chunk.js"
+  },
+  {
+    "revision": "2f9d8defe62313379f50",
+    "url": "/static/keji/static/js/3.47ca4458.chunk.js"
+  },
+  {
+    "revision": "bd257c6caadbc9aabe4f",
+    "url": "/static/keji/static/js/4.1b377015.chunk.js"
+  },
+  {
+    "revision": "7e17e3999518b92aa089",
+    "url": "/static/keji/static/js/5.3a8ece0f.chunk.js"
+  },
+  {
+    "revision": "79b47e2436f9cb1b0bdc",
+    "url": "/static/keji/static/js/main.5d167ab0.chunk.js"
+  },
+  {
+    "revision": "2caac09fbffb375016fc",
+    "url": "/static/keji/static/js/runtime-main.53088917.js"
+  },
+  {
+    "revision": "efc13f07ec84cc7ace3e49bf25e09d82",
+    "url": "/static/keji/static/media/homeCircle.efc13f07.png"
+  },
+  {
+    "revision": "634d0e17d073dd9988f95fbe27f29db5",
+    "url": "/static/keji/static/media/mainBackground.634d0e17.png"
+  },
+  {
+    "revision": "a35d6c22fe1342b342588b26eff19131",
+    "url": "/static/keji/static/media/mainBox.a35d6c22.png"
+  }
+]);
